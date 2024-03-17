dd54735df173fe795d42e6c4c1325f2d43b884d1
Yevhenii <senusheugen@gmail.com>
2024-03-17 15:24:40 +0100

    lab2 source code

---
+++ lab_2.0.0/simulation.cc
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+/**
+ * Derive the Txc1 class from cSimpleModule. In the Tictoc1 network,
+ * both the `tic' and `toc' modules are Txc1 objects, created by OMNeT++
+ * at the beginning of the simulation.
+ */
+class Simulation : public cSimpleModule
+{
+  protected:
+    // The following redefined virtual function holds the algorithm.
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+// The module class needs to be registered with OMNeT++
+Define_Module(Simulation);
+
+void Simulation::initialize()
+{
+
+}
+
+void Simulation::handleMessage(cMessage *msg)
+{
+
+}
